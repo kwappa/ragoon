b3cd174114dad1ad349a21c2f792785d8b366b9d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-15 15:07:06 +0900

    :secret: enable to specify garoon endpoint / username / password
    * by ENV (recommended)
    * by `./.secret_options`
      * write with ruby hash syntax. it will be evaled.

--- lib/ragoon.rb
+++ lib/ragoon.rb
@@ -1,4 +1,31 @@
 require 'ragoon/version'
 
 module Ragoon
+  @@secret_options = {}
+
+  def self.garoon_endpoint
+    ENV['GAROON_ENDPOINT'] || secret_options[:garoon_endpoint] || raise_option_error('endpoint')
+  end
+
+  def self.garoon_username
+    ENV['GAROON_USERNAME'] || secret_options[:garoon_username] || raise_option_error('username')
+  end
+
+  def self.garoon_password
+    ENV['GAROON_PASSWORD'] || secret_options[:garoon_password] || raise_option_error('password')
+  end
+
+  def self.raise_option_error(type)
+    raise "must specify garoon_#{type} by ENV or `./.secret_options`."
+  end
+
+  private
+
+  def self.secret_options
+    if @@secret_options.empty?
+      raise '`./.secret_options` is required.' unless File.exists?('./.secret_options')
+      @@secret_options = eval(File.read('./.secret_options'))
+    end
+    @@secret_options
+  end
 end

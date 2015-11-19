d420fd592b55fecd77427a5c2417982bfdecea78
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-19 22:20:25 +0900

    :pencil: enable to query event summary from api response

--- lib/ragoon/services/schedule.rb
+++ lib/ragoon/services/schedule.rb
@@ -13,7 +13,35 @@ class Ragoon::Services::Schedule < Ragoon::Services
     body_node.add_child(parameter_node)
 
     client.request(action_name, body_node)
-    client
+
+    events = client.result_set.xpath('//schedule_event').find_all { |ev| ev[:event_type] == 'normal' }.map do |event|
+      public_event = event[:public_type] == 'public'
+      {
+        title:    public_event ? event[:detail] : '予定あり',
+        period:   start_and_end(event),
+        facility: public_event ? facility_names(event) : '',
+      }
+    end
+  end
+
+  def facility_names(event)
+    event.xpath('ev:members', ev: "http://schemas.cybozu.co.jp/schedule/2008").
+      children.map { |c| c.xpath('ev:facility', ev: "http://schemas.cybozu.co.jp/schedule/2008").first }.
+      compact.map { |n| n[:name] }
+  end
+
+  def start_and_end(event)
+    if event[:allday] == 'true'
+      '終日'
+    else
+      period = event.children.xpath('ev:datetime', ev: "http://schemas.cybozu.co.jp/schedule/2008").first
+
+      return 'error' if period.nil?
+
+      start_time = Time.parse(period[:start]).localtime.strftime('%R')
+      end_time = event[:start_only] == 'true' ? '' : Time.parse(period[:end]).localtime.strftime('%R')
+      "#{start_time}〜#{end_time}"
+    end
   end
 
   def default_options(action_name)

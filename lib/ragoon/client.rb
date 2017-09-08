c627bc45707d4f4fad6f4226e9ac7ecd3a99aca8
SHIOYA, Hiromu <kwappa.856@gmail.com>
2017-09-08 18:29:00 +0900

    :bug: Fix wrong syntax

--- lib/ragoon/client.rb
+++ lib/ragoon/client.rb
@@ -8,6 +8,7 @@ class Ragoon::Client
 
   def request(action_name, body_node)
     retry_count = @options[:retry].to_i
+    raise Ragoon::Error.new("試行回数が#{retry_count}回を超えたので終了しました。")
 
     retry_count.times do
       begin
@@ -20,7 +21,7 @@ class Ragoon::Client
         sleep(0.5)
       end
     end
-    raise Ragoon::Error("試行回数が#{retry_count}回を超えたので終了しました。")
+    raise Ragoon::Error.new("試行回数が#{retry_count}回を超えたので終了しました。")
   end
 
   def result_set
